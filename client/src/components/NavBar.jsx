d0230724bd741ed2f1bb8d5130ac0bac7bc7ee6b
chandrabhan <chandrabhansingh27d@gmail.com>
2024-02-08 23:55:08 +0530

    fix: New Logo; feat: DB Connection

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,15 @@
-import { NavLink, Outlet } from "react-router-dom";
-import logo from "../assets/logo/svg/logo-no-background.svg";
+import { NavLink } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { FaRegUserCircle, FaRegHeart } from "react-icons/fa";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { GoSearch } from "react-icons/go";
+import { useNavigate } from 'react-router-dom';
+import logo from "../assets/logo_new/svg/logo-no-background.svg";
 
 const NavBar = () => {
+    //hooks call
+    const navigateTo = useNavigate();
+
     //Navigation items
     const navItems = [
         {
@@ -23,21 +31,37 @@ const NavBar = () => {
 
     ];
 
+    const goToHomepage = () => {
+        navigateTo('/')
+    }
 
     return (
         <>
-            <header>
-                <nav className="flex ">
-                    <div className="w-[12%] h-[12%] mt-3">
-                        <img src={logo} alt="logo" />
-                    </div>
-                    <ul>
+            <header className="flex justify-between items-center py-1 px-1">
+                <div className="w-[12%] h-[12%] cursor-pointer" onClick={goToHomepage}>
+                    <img src={logo} alt="logo" />
+                </div>
+                <nav>
+                    <ul className="flex gap-4 justify-center">
                         {navItems.map(navItem => (
                             <NavLink to={navItem.link} key={navItem.title}>{navItem.title}</NavLink>
                         ))}
                     </ul>
                 </nav>
+                <div id="user-control" className="flex justify-between gap-1 items-center mr-4 w-1/4 ">
+                    <div className="flex items-center w-3/4 border border-gray-200 rounded bg-gray-100">
+                        <GoSearch size={25} color="#3f3f46" className="p-0.5 mx-0.5" />
+                        <input className="focus:border-none focus:outline-none  focus:bg-white p-1 w-full text-sm m-px" type="text" name="search" id="serach-box" placeholder="Search for products, categories, etc." />
+                    </div>
+                    <IconContext.Provider value={{ className: "cursor-pointer", size: 20 }}>
+                        <>
+                            <FaRegUserCircle />
+                            <FaRegHeart />
+                            <HiOutlineShoppingBag />
+                        </>
+                    </IconContext.Provider>
 
+                </div>
             </header>
         </>
     );
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv/config";
+import {connectToDB, getDB } from './db.js';
 
 const app = express();
 
@@ -9,19 +10,44 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(require("./routes/record"));
+let db;
+// app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+// const dbo = require("./db/conn");
 
-app.get('/', (req, res) => {
-    res.status(200).send(`<h1>Ecommerce Store Backend</h1>`)
+connectToDB((err) => {
+    if(!err) {
+        app.listen(port, () => {
+            // perform a database connection when server starts
+            
+            //: ${port} dbo.connectToServer(function (err) {
+            //     if (err) console.error(err);
+            // });
+            console.log(`Server is running on port: ${port}`);
+        });
+        db = getDB();
+    }
 })
 
-app.listen(port, () => {
-    // perform a database connection when server starts
+
+
+
+
+app.get('/', (req, res) => {
+    let users = [];
+
+    db.collection('collection1')
+        .find()
+        .project({_id: 0})
+        .sort({name: 1})
+        .forEach(user => users.push(user))
+        .then(() => {
+            res.status(200).json(users)
+        })
+        .catch(() => {
+            res.status(500).json({
+                error: "Could not fetch the users"
+            })
+        })
     
-    //: ${port} dbo.connectToServer(function (err) {
-    //     if (err) console.error(err);
-    // });
-    console.log(`Server is running on port: ${port}`);
-});
+})
